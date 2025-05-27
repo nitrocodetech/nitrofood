97275b43f813a1e30b8ce409cffd03fa5fff5ed6
ammad amin <153178783+ammad-amin-1999313@users.noreply.github.com>
2025-05-27 19:47:33 +0500

    fix: structure

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginPage from "@/components/home/login";
-
-export default function Home() {
-  return (
-    <div>
-      <LoginPage />
-    </div>
-  );
-}
--- components/home/login/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const LoginPage = () => {
-  return <div className="py-8"></div>;
-};
-
-export default LoginPage;
