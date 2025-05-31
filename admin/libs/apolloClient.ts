acfc026f5c9b65b77c356165ec6319af2d416a10
hamzamushtaq840 <hamzamushtaq840@gmail.com>
2025-06-01 02:10:33 +0500

    fix: change url

--- admin/libs/apolloClient.ts
+++ admin/libs/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:8080/graphql",
+  uri: "http://nitrofood-production.up.railway.app/graphql", // replace with your actual GraphQL endpoint
   cache: new InMemoryCache(),
 });
 
