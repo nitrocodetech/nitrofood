414d18d464c2ecd08760338da87092a9eb92e5db
hamzamushtaq840 <hamzamushtaq840@gmail.com>
2025-06-03 02:06:51 +0500

    feat: add restaurant

--- backend/src/restaurants/restaurants.module.ts
+++ backend/src/restaurants/restaurants.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RestaurantsService } from './restaurants.service';
 import { RestaurantsResolver } from './restaurants.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from './entities/restaurant.entity';
 
 @Module({
-  providers: [RestaurantsResolver, RestaurantsService],
+  imports: [TypeOrmModule.forFeature([Restaurant])],
+  providers: [RestaurantsService, RestaurantsResolver],
+  exports: [RestaurantsService],
 })
 export class RestaurantsModule {}
